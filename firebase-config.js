6861220665e71747533cdc71e86d905aa5ce531d
alinaresblanco-gif <alinares.blanco@gmail.com>
2026-09-13 10:28:16 +0200

    ghg

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 self.MY_FAMILY_FIREBASE = {
   config: {
-    apiKey: 'REEMPLAZAR_API_KEY',
-    authDomain: 'REEMPLAZAR_PROJECT_ID.firebaseapp.com',
-    projectId: 'REEMPLAZAR_PROJECT_ID',
-    storageBucket: 'REEMPLAZAR_PROJECT_ID.firebasestorage.app',
-    messagingSenderId: 'REEMPLAZAR_MESSAGING_SENDER_ID',
-    appId: 'REEMPLAZAR_APP_ID'
+    apiKey: 'AIzaSyCM8yAwpPtM_K3rEJ7ngUwJun-AfWukVjg',
+    authDomain: 'my-family-9086c.firebaseapp.com',
+    projectId: 'my-family-9086c',
+    storageBucket: 'my-family-9086c.firebasestorage.app',
+    messagingSenderId: '557032783606',
+    appId: '1:557032783606:web:aeb552dffbef2b3d20f6d7'
   },
-  vapidKey: 'REEMPLAZAR_CLAVE_PUBLICA_VAPID'
+  vapidKey: 'BAj0TIj0i8puk5g5S4Pu9nXsEXPDvJIItNAqXygjimvx7ktRQvXSPZjKxoSI6su2rTlJ6J0AItmxGBYJtBPWKqU'
 };
