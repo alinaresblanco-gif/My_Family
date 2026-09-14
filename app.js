b197351166e2fe852f3bd45f62d4bc0cb416b12f
alinaresblanco-gif <alinares.blanco@gmail.com>
2026-09-14 18:23:25 +0200

    asa

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const FALLBACK_APP_VERSION = '2026.09.09.2';
-const SHEETS_API_URL = 'https://script.google.com/macros/s/AKfycbx1iCtC79EKo80xfYvx80LuGHhELDuDDVRqBI9S7OlRv44xeqYzw1iQq4qMPg2_MyI/exec';
+const SHEETS_API_URL = 'https://script.google.com/macros/s/AKfycby9HBtQGCzd0C24amX9ND3ot87rNiTIKMEKiF0bvWgUNUbNuYNr8ayzhp26-3UPKiM4/exec';
 const FAMILY_ID = 'family-my-family';
 const PUSH_DEVICE_ID_KEY = 'my-family-push-device-id';
 let currentAppVersion = null;
